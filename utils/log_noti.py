3cb4d3d6137bc16fe356b120951d65d8b9bac337
LeTienQuyet <21520428@gm.uit.edu.vn>
2025-11-06 16:45:29 +0700

    show the training loss and validation metric

---
+++ utils/log_noti.py
@@ -0,0 +1,16 @@
+def log_table(epoch, train_mse_loss, train_percept_loss, train_lossGen, avg_val_psnr, avg_val_ssim, width=53):
+    # Epoch
+    print("-" * width)
+    epoch_str = f"Epoch = {epoch}"
+    print(f"|{epoch_str:^{width - 2}}|")
+    print("-" * width)
+
+    # Train loss
+    loss_str = f"MSE: {train_mse_loss:.4f}    Percept: {train_percept_loss:.4f}   Gen: {train_lossGen:.4f}"
+    print(f"|{loss_str:^{width - 2}}|")
+    print("-" * width)
+
+    # Validation metrics
+    metric_str = f"PSNR: {avg_val_psnr:.4f}   SSIM: {avg_val_ssim:.4f}"
+    print(f"|{metric_str:^{width - 2}}|")
+    print("-" * width)
