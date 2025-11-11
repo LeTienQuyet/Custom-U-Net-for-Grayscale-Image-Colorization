a611c42314e4e5227932be1acf42c352eeafab31
LeTienQuyet <21520428@gm.uit.edu.vn>
2025-11-11 10:06:59 +0700

    final prepare data

--- utils/dataset.py
+++ utils/dataset.py
@@ -5,29 +5,28 @@ from torchvision import transforms
 from torch.utils.data import Dataset, DataLoader
 
 class ColorizationDataset(Dataset):
-    def __init__(self, root="./data", split="train", img_size=256):
+    def __init__(self, root="./data", split="train", img_size=128):
         self.root = root
         self.split = split
         self.filenames = os.listdir(os.path.join(root, split))
 
         if split == "train":
-            self.transform = transforms.Compose([
+            self.resize = transforms.Compose([
                 transforms.Resize((img_size, img_size)),
                 transforms.RandomHorizontalFlip()
             ])
         else:
-            self.transform = transforms.Compose([
-                transforms.Resize((img_size, img_size)),
-            ])
+            self.resize = transforms.Resize((img_size, img_size))
 
         self.gray_transform = transforms.Compose([
+            transforms.Grayscale(num_output_channels=1),
             transforms.ToTensor(),
-            transforms.Normalize(mean=[0.5], std=[0.5])
+            transforms.Normalize(mean=(0.5, ), std=(0.5, ))
         ])
 
         self.color_transform = transforms.Compose([
             transforms.ToTensor(),
-            transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+            transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))
         ])
 
     def __len__(self):
@@ -38,17 +37,15 @@ class ColorizationDataset(Dataset):
         file_path = os.path.join(self.root, self.split, filename)
 
         img = Image.open(file_path).convert("RGB")
-        img = self.transform(img)
+        img = self.resize(img)
 
-        gray_img = transforms.Grayscale(num_output_channels=1)(img)
-
-        gray_tensor = self.gray_transform(gray_img)
+        gray_tensor = self.gray_transform(img)
         color_tensor = self.color_transform(img)
 
-        return gray_tensor, color_tensor
+        return gray_tensor, color_tensor  
 
-def prepare_data(root="./data", split="train", batch_size=64, img_size=256):
+def prepare_data(root="./data", split="train", batch_size=64, img_size=128):
     dataset = ColorizationDataset(root, split, img_size)
     shuffle = True if split == "train" else False
-    dataloader = DataLoader(dataset, batch_size, shuffle)
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=2)
     return dataloader
