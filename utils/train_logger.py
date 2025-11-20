05976e845c03bcc4270c78f98cfa592af649d728
LeTienQuyet <21520428@gm.uit.edu.vn>
2025-11-20 09:29:58 +0700

    refactor (train_logger): unify logging logic for cases with/without train_gen_loss

--- utils/train_logger.py
+++ utils/train_logger.py
@@ -1,33 +1,19 @@
-# def log_table(epoch, train_mse_loss, train_percept_loss, train_gen_loss, avg_val_psnr, avg_val_ssim, width=53):
-#     # Epoch
-#     print("-" * width)
-#     epoch_str = f"Epoch = {epoch}"
-#     print(f"|{epoch_str:^{width - 2}}|")
-#     print("-" * width)
-#
-#     # Train loss
-#     loss_str = f"Pixel: {train_mse_loss:.4f}    Perceptual: {train_percept_loss:.4f}   Gen: {train_gen_loss:.4f}"
-#     print(f"|{loss_str:^{width - 2}}|")
-#     print("-" * width)
-#
-#     # Validation metrics
-#     metric_str = f"PSNR: {avg_val_psnr:.4f}   SSIM: {avg_val_ssim:.4f}"
-#     print(f"|{metric_str:^{width - 2}}|")
-#     print("-" * width)
-
-def epoch_summary(epoch, train_mse_loss, train_percept_loss, avg_val_psnr, avg_val_ssim, width=53):
+def epoch_summary(epoch, avg_val_psnr, avg_val_ssim, train_mse_loss, train_percept_loss, train_gen_loss=None, width=53):
+    line = "-" * width
     # Epoch
-    print("-" * width)
+    print(line)
     epoch_str = f"Epoch = {epoch}"
     print(f"|{epoch_str:^{width - 2}}|")
-    print("-" * width)
+    print(line)
 
     # Train loss
     loss_str = f"Pixel: {train_mse_loss:.4f}    Perceptual: {train_percept_loss:.4f}"
+    if train_gen_loss is not None:
+        loss_str += f"    Gen: {train_gen_loss:.4f}"
     print(f"|{loss_str:^{width - 2}}|")
-    print("-" * width)
+    print(line)
 
     # Validation metrics
     metric_str = f"PSNR: {avg_val_psnr:.4f}   SSIM: {avg_val_ssim:.4f}"
     print(f"|{metric_str:^{width - 2}}|")
-    print("-" * width)
+    print(line)
